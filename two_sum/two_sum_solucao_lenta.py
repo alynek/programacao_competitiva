8302ef9c4b6b7151e179c6dabb7bde36f70995f6
alynek <alyne0x67@gmail.com>
2025-03-08 09:37:37 -0300

    Cria esboço da função

---
+++ two_sum/two_sum_solucao_lenta.py
@@ -0,0 +1,11 @@
+def two_sum(numeros, k):
+
+    # for p1 in range(len(numeros)):
+    #     for p2 in range(len(p1)):
+    #         if (k - numeros[p1]) == k:
+    #             print('achou')
+    #             break
+
+    print('[]')
+
+two_sum([1, 3, 7, 9, 2], 25)
