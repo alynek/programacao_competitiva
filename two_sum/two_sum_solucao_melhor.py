5c9ad122c50c5e6869c935ed877034f7ba8b6dec
alynek <alyne0x67@gmail.com>
2025-03-09 23:47:25 -0300

    Melhora complexidade de tempo do algortimo

---
+++ two_sum/two_sum_solucao_melhor.py
@@ -0,0 +1,29 @@
+def two_sum(numeros, k):
+
+    if len(numeros) > 1: 
+                                                      
+        dictionary = {}                                
+        
+        for item in range(len(numeros)):                
+            numeroAEncontrar = k - numeros[item]        
+            dictionary[numeroAEncontrar] = item
+            proximo_item = item + 1                    
+
+            if proximo_item < len(numeros):    
+                x = numeros[proximo_item]
+                if x in dictionary:
+                    return [dictionary[x], proximo_item]
+
+    return None
+
+print(two_sum([1, 3, 7, 9, 2], 11))   #saída esperada: [3 4]
+print(two_sum([1, 3, 7, 9, 2], 8))   #saída esperada: [0 2]
+print(two_sum([1, 3, 7, 9, 2], 10))   #saída esperada: [1 2]
+print(two_sum([1, 3, 7, 9, 2], 3))   #saída esperada: [0 4]
+print(two_sum([1, 3, 7, 9, 2], 12))   #saída esperada: [1 3]
+print(two_sum([1, 3, 7, 9, 2], 25))    #saída esperada: None
+print(two_sum([5], 25))                #saída esperada: None 
+print(two_sum([], 25))                 #saída esperada: None 
+
+#complexidade de tempo: O(n), sendo n o tamanho da lista
+#complexidade  de espaço: O(1), isso porque não temos a criação de nenhuma variável que escala
