d061492f6e6db0eaf7c6623369284a38dcbd300a
alynek <alyne0x67@gmail.com>
2025-03-11 21:41:57 -0300

    Remove comentários e altera nome do arquivo

--- two_sum/solucao_melhor.py
+++ two_sum/solucao_melhor.py
@@ -15,8 +15,5 @@ def two_sum(numeros, k):
 
     return None
 
-print(two_sum([5], 25))                #saída esperada: None 
-print(two_sum([], 25))                 #saída esperada: None 
-
 #complexidade de tempo: O(n), sendo n o tamanho da lista
 #complexidade  de espaço: O(1), isso porque não temos a criação de nenhuma variável que escala
