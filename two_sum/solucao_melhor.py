d221d898547700f807bf605a68ca1d3dad12ce42
alynek <alyne0x67@gmail.com>
2025-03-11 21:40:52 -0300

    Ajusta nomes das classes

---
+++ two_sum/solucao_melhor.py
@@ -0,0 +1,22 @@
+def two_sum(numeros, k):
+
+    if len(numeros) > 1: 
+                                                      
+        dicionario_numeros = {}                                
+        
+        for indice, elemento in enumerate(numeros):  
+
+
+            if elemento in dicionario_numeros:                  
+                return [dicionario_numeros[elemento], indice]
+            
+            numero_para_encontrar = k - numeros[indice]     
+            dicionario_numeros[numero_para_encontrar] = indice     
+
+    return None
+
+print(two_sum([5], 25))                #saída esperada: None 
+print(two_sum([], 25))                 #saída esperada: None 
+
+#complexidade de tempo: O(n), sendo n o tamanho da lista
+#complexidade  de espaço: O(1), isso porque não temos a criação de nenhuma variável que escala
